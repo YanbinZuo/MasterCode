27f1e6863dfc82928cbd39555672bc6fe9dd1070
Yanbin Zuo <jimmyzuo123456@gmail.com>
2023-09-30 23:48:09 -0700

    update mergeSortedArray

--- section6_Array/75-mergeSortedArrays.js
+++ section6_Array/75-mergeSortedArrays.js
@@ -1,24 +1,29 @@
 function mergeSortedArrays(arr1, arr2) {
-    let i = 0, j = 0;
-    const merged = [];
-    while(i<arr1.length && j< arr2.length) {
-        if(arr1[i] < arr2[j]) {
-            merged.push(arr1[i])
-            i++;
-        } else {
-            merged.push(arr2[j])
-            j++;
-        }
+  if (arr1.length === 0) return arr2;
+  if (arr2.length === 0) return arr1;
+
+  let i = 0,
+    j = 0;
+  const merged = [];
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      merged.push(arr1[i]);
+      i++;
+    } else {
+      merged.push(arr2[j]);
+      j++;
     }
-    if(i=== arr1.length) {
-        merged.push(...arr2.slice(j))
-    } else if( j === arr2.length) {
-        merged.push(...arr1.slice(i));
-    }
-    return merged;
+  }
+  if (i === arr1.length) {
+    merged.push(...arr2.slice(j));
+  } else if (j === arr2.length) {
+    merged.push(...arr1.slice(i));
+  }
+  return merged;
 }
 
-const mergeSortedArraysV2 = (arr1, arr2) => [...arr1, ...arr2].sort((a,b) => a - b)
+const mergeSortedArraysV2 = (arr1, arr2) =>
+  [...arr1, ...arr2].sort((a, b) => a - b);
 
-console.log(mergeSortedArrays([0,3,4,69], [0,3,4,6,30]));
-console.log(mergeSortedArraysV2([0,3,4,69], [0,3,4,6,30]));
+console.log(mergeSortedArrays([0, 3, 4, 69], [0, 3, 4, 6, 30]));
+console.log(mergeSortedArraysV2([0, 3, 4, 69], [0, 3, 4, 6, 30]));
