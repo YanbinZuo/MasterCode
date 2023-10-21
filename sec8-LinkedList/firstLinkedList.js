77a37c30fa7b36b187b76172e3c86511be0c22ba
Yanbin Zuo <jimmyzuo123456@gmail.com>
2023-10-21 15:02:02 -0700

    add insert

--- sec8-LinkedList/firstLinkedList.js
+++ sec8-LinkedList/firstLinkedList.js
@@ -12,6 +12,12 @@
 //     }
 //   }
 // };
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }    
+}
 
 class LinkedList {
     constructor(value) {
@@ -24,34 +30,82 @@ class LinkedList {
     }
 
     append(value) {
-        this.tail.next = {
-            value: value,
-            next: null
-        }
-        this.tail = this.tail.next;
+        // this.tail.next = {
+        //     value: value,
+        //     next: null
+        // }
+        // this.tail = this.tail.next;
+        const newNode = new Node(value);
+        this.tail.next = newNode;
+        this.tail = newNode;
         this.length++;
-        return this;
+        // return this;
+        return this.printList();
     }
 
     prepend(value) {
-        const newNode = {
-            value: value,
-            next: this.head
-        }
+        // const newNode = {
+        //     value: value,
+        //     next: this.head
+        // }
+        const newNode = new Node(value);
+        newNode.next = this.head;
         this.head = newNode;
         this.length++;
+        return this.printList();
+    }
+
+    printList() {
+        const arr = [];
+        let currNode = this.head;
+        while(currNode !== null) {
+            arr.push(currNode.value);
+            currNode = currNode.next;
+        }
+        console.log(arr);
+    }
+
+    insert(index, value) {
+        if(index === 0) {
+            this.prepend(value);
+            return;
+        }
+        if(index >= this.length) {
+            this.append(value);
+            return;
+        }
+        const newNode = new Node(value);
+        const prevNode = this.traverseToIndex(index - 1);
+        newNode.next = prevNode.next;
+        prevNode.next = newNode;
+        this.length++;
+        return this.printList();
+    }
+
+    traverseToIndex(index) {
+        // check params
+        if(index >= this.length - 1) return this.tail;
+        
+        let counter = 0;
+        let currNode = this.head;
+        while(counter < index) {
+            currNode = currNode.next;
+            counter++;
+        }
+        return currNode;
     }
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.insert(1, 2)
+myLinkedList.insert(0, 0)
+
 
 console.log(myLinkedList);
-
 const util = require('util');
 // use util.inspect to show the entire structure when do console.log()
-console.log(util.inspect(myLinkedList, { showHidden: false, depth: null }));
-
-myLinkedList.prepend(1);
+// console.log(util.inspect(myLinkedList, { showHidden: false, depth: null }));
 console.log(util.inspect(myLinkedList, {showHidden: false, depth: null}))
